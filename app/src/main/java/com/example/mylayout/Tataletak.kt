c340a7b16e3bf4a331072627298043ade3c61a44
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2025-10-16 16:19:37 +0700

    Membuat function TataletakColumn composable

--- app/src/main/java/com/example/mylayout/Tataletak.kt
+++ app/src/main/java/com/example/mylayout/Tataletak.kt
@@ -23,3 +23,13 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.material3.Text
 
+@Composable
+fun TataletakColumn(modifier: Modifier = Modifier) {
+    Column (modifier = modifier.padding(top = 20.dp, start = 20.dp, end = 20.dp)){
+        Text(text = "Komponen1")
+        Text(text = "Komponen2")
+        Text(text = "Komponen3")
+        Text(text = "Komponen4")
+    }
+}
+
