fb1b062a3b0f6543e75759d9e6012835b1aa706c
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2025-10-16 16:27:53 +0700

    TataletakColumnRow composable

--- app/src/main/java/com/example/mylayout/Tataletak.kt
+++ app/src/main/java/com/example/mylayout/Tataletak.kt
@@ -97,6 +97,24 @@ fun TataletakRowColumn(modifier: Modifier = Modifier) {
     }
 }
 
-
+@Composable
+fun TataletakColumnRow(modifier: Modifier = Modifier) {
+    Column() {
+        //Baris1
+        Row(modifier = modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly) {
+            Text(text = "Komponen1Baris1")
+            Text(text = "Komponen2Baris1")
+            Text(text = "Komponen3Baris1")
+        }
+        //Baris2
+        Row(modifier = modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly) {
+            Text(text = "Komponen1Baris2")
+            Text(text = "Komponen2Baris2")
+            Text(text = "Komponen3Baris2")
+        }
+    }
+}
 
 
