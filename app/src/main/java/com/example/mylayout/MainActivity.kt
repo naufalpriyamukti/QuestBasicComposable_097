ef1bb9ba4acd9977eecb4070c3f37ce7fe3f8a17
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2025-10-16 16:07:07 +0700

    Setup MainActivity sesuai modul

--- app/src/main/java/com/example/mylayout/MainActivity.kt
+++ app/src/main/java/com/example/mylayout/MainActivity.kt
@@ -13,35 +13,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mylayout.ui.theme.MyLayoutTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            MyLayoutTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+        setContent{
+            Scaffold(modifier = Modifier.fillMaxSize()) {InnerPadding ->
+                // Panggil composable layout utama dengan padding dari Scaffold
+                TataletakBoxColumnRow( modifier = Modifier.padding(paddingValues = InnerPadding))}
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyLayoutTheme {
-        Greeting("Android")
     }
 }
